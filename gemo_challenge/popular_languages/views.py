8853234d3037ce95b0e50578b81395a7512f9a7c
Lina Dosse <bennani.d.lina@gmail.com>
2020-07-31 19:49:18 +0100

    check response status code

--- gemo_challenge/popular_languages/views.py
+++ gemo_challenge/popular_languages/views.py
@@ -20,20 +20,23 @@ def list_languages(request):
     url = "https://api.github.com/search/repositories?q=created:>{0}&sort=stars&order=desc&page=1&per_page=100".format(
         thirty_days_ago)
     response = requests.get(url)
-    trending_repositories = response.json()['items']
-    list_languages = {}
-    NO_OF_REPOS = "No of repos"
-    LIST_OF_REPOS = "List of repos"
-    URL = "url"
-    HTML_URL = "html_url"
+    if response.status_code == 200:
+        trending_repositories = response.json()['items']
+        list_languages = {}
+        NO_OF_REPOS = "No of repos"
+        LIST_OF_REPOS = "List of repos"
+        URL = "url"
+        HTML_URL = "html_url"
 
-    for repo in trending_repositories:
-        language = repo['language']
-        prevEntry = list_languages.setdefault(language,
-                                              {NO_OF_REPOS: 0,
-                                               LIST_OF_REPOS: []})
+        for repo in trending_repositories:
+            language = repo['language']
+            prevEntry = list_languages.setdefault(language,
+                                                  {NO_OF_REPOS: 0,
+                                                   LIST_OF_REPOS: []})
 
-        list_languages[language][NO_OF_REPOS] = prevEntry[NO_OF_REPOS] + 1
-        prevEntry[LIST_OF_REPOS].append({repo[URL], repo[HTML_URL]})
+            list_languages[language][NO_OF_REPOS] = prevEntry[NO_OF_REPOS] + 1
+            prevEntry[LIST_OF_REPOS].append({repo[URL], repo[HTML_URL]})
 
-    return Response(list_languages)
+        return Response(list_languages)
+
+    return Response(response, status=response.status_code)
